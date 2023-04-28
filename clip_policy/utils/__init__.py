a171255e6ece151182f1528a1f508bf32e085b5b
RaiAnant <anant.v.rai@gmail.com>
2023-04-28 19:43:56 -0400

    added set seed everywhere

--- clip_policy/utils/__init__.py
+++ clip_policy/utils/__init__.py
@@ -1,2 +1,13 @@
 from .constants import *
 from .word_extractor import *
+import torch
+import numpy as np
+import random
+from datetime import timedelta
+
+def set_seed_everywhere(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
